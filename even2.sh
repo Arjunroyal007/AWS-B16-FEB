0638286858b1fa14e9d2a48c55b44254ca7c4771
arjun <arjunroyal916@gmail.com>
2024-04-18 03:10:36 +0000

    addded a even num logic

--- even2.sh
+++ even2.sh
@@ -1,4 +1,7 @@
 read -p "enter the number:" NUM
 echo "entered nuber is:" $NUM
-#added comment section
-#to check even number
+if [expr $NUM/2 = 0]
+	echo $NUM "is even number"
+then
+	echo $NUM "is odd number"
+fi
